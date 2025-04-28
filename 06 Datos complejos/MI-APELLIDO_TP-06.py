df07dacfafb0409420f2c8cc0d3fc47a9a497ce9
arturokaadu <arturokaadu@gmail.com>
2025-04-28 14:53:10 -0300

    Listas TP

---
+++ 06 Datos complejos/MI-APELLIDO_TP-06.py
@@ -0,0 +1,60 @@
+#LISTAS TP
+""" numeros = list(range(4,101,4))
+print(numeros) """
+
+#2#
+
+""" elementos = [1.4, True, "Hola", 2, 300]
+print(elementos[-2]) """
+
+#3#
+""" emptyList = []
+emptyList.extend(["Hola", "chau", "adios"])
+print(emptyList) """
+
+#4#
+""" animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales) """
+
+#5#
+""" numeros = [8,15,3,22,7]
+numeros.remove(max(numeros))# elimina el maximo entre esos numeros de la lista
+
+print(numeros)
+ """
+ 
+#6#
+""" saltos = list(range(10, 31,5))
+print(saltos[0: 2]) """
+
+#7#
+""" autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "fiesta"
+autos[2] = "focus"
+print(autos) """
+
+#8#
+""" dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles) """
+
+#9#
+""" compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras) """
+
+#10#
+""" lista_anidada = []
+lista_anidada.extend([15, True, [25.5, 57.9,30.6], [False]])
+print(lista_anidada[0])
+print(lista_anidada[1])
+print(lista_anidada[2][0])
+print(lista_anidada[2][1])
+print(lista_anidada[2][2])
+print(lista_anidada[3]) """
